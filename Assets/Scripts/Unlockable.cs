ed07ed67e0725365ac1a6244bc6b01b9be5456f9
njaramillo-ng <njaramillo@nemorisgames.com>
2016-10-04 10:28:02 -0300

    Jumper, Diver, Platform scripts + prefabs

---
+++ Assets/Scripts/Unlockable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unlockable : MonoBehaviour 
+{
+	public int id; //identificador
+	public string name; //nombre
+	public string description; //descripcion
+	public int tier; //tier de unlockables al que pertenece
+
+}
