a473dbe4a4328f65338f42221b48e9ba3d967cf7
njaramillo-ng <njaramillo@nemorisgames.com>
2016-10-11 13:07:36 -0300

    Added platform and jumper, can instantiate any type

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,61 +5,109 @@ using System.Collections;
 public class GameController : MonoBehaviour 
 {
 
+	Transform diverPos;
+
 	[Header("Diver")]
 	public GameObject diverGameObject; //diver en uso
-	public Vector3 jumpForce; //vector fuerza para el salto
+	public Vector3 diverJumpForce; //vector fuerza para el salto
 	public float normalHorizontalSpeed; //velocidad horizontal normal del diver
 	public float trickHorizontalSpeed; //velocidad horizontal en truco del diver
 	public float verticalSpeed;
 
-	Vector3 diverInitialPosition; //posicion inicial del diver
-	Quaternion diverInitialRotation; //rotacion inicial del diver
 	Rigidbody diverRigidbody; //rigidbody del diver
-	Diver diver; //diver (script) del diver (parámetros)
+	Diver diverProps; //diver (script) del diver (parámetros)
+	GameObject diver;
 	float jumpStart;
 	float jumpEnd;
+	bool controllingDiver;
 
 	float flips = 0;
 	float deltaRotation = 0;
 	float currentRotation = 0;
 	float WindupRotation = 0;
 
+	[Header("Jumper")]
+	public Vector3 jumperJumpForce;
+	public GameObject jumperGameObject;
+
+	GameObject jumper;
+	bool controllingJumper;
+	Rigidbody jumperRigidbody;
+	Vector3 jumperPos;
+
+	[Header("Platform")]
+	public GameObject platformGameObject;
+
+	GameObject platform;
+
+
 	// Use this for initialization
-	void Start () 
+	void Awake () 
 	{
-		diverRigidbody = diverGameObject.GetComponent<Rigidbody> ();
-		diver = diverGameObject.GetComponent<Diver> ();
-		diverInitialPosition = diverRigidbody.position;
-		diverInitialRotation = diverRigidbody.rotation;
-		normalHorizontalSpeed = jumpForce.x / 50;
-		trickHorizontalSpeed = normalHorizontalSpeed / 2;
-		diverRigidbody.maxAngularVelocity = 100;
-		verticalSpeed = diver.trickSpinSpeed / 100;
+		SetUp ();
 	}
 
 	void FixedUpdate(){
-		if (diver.onGround && Input.GetKeyDown (KeyCode.Space)) 
-		{
-			flips = 0;
-			WindupRotation = 0;
-			DiverJump (jumpForce);
-		};
 
+		//when controlling Jumper
+		if (controllingJumper && !controllingDiver) 
+		{
+			if (Input.GetKeyDown (KeyCode.Space)) 
+			{
+				jumperRigidbody.AddForce (jumperJumpForce);
+				controllingJumper = false;
+			}
+		}
+
+		//when controlling Diver
+		if(!controllingJumper && controllingDiver){
+			if (diverProps.onGround && Input.GetKeyDown (KeyCode.Space)) 
+			{
+				flips = 0;
+				WindupRotation = 0;
+				DiverJump (diverJumpForce);
+			};
+			if (!diverProps.onGround) 
+			{
+				SetDiverSpinSpeed (diverProps.spinSpeed, diverProps.trickSpinSpeed);
+			};
+		}
+
+		//Cleanup
 		if (diverRigidbody.position.y < -1) 
 		{
 			ResetDiverPositionRotation ();
+			jumperRigidbody.position = jumperPos;
+			controllingJumper = true;
+			controllingDiver = false;
 		}
-
-		if (!diver.onGround) 
-		{
-			SetDiverSpinSpeed (diver.spinSpeed, diver.trickSpinSpeed);
-		};
-
 	}
 
-	void Update()
+	void SetUp()
 	{
-		
+		//Set up diver
+		diverPos = transform.FindChild ("DiverPos").transform;
+
+		diver = (GameObject)Instantiate(diverGameObject,diverPos.position,diverPos.rotation);
+		diverRigidbody = diver.GetComponent<Rigidbody> ();
+		diverProps = diver.GetComponent<Diver> ();
+		normalHorizontalSpeed = diverJumpForce.x / 50;
+		trickHorizontalSpeed = normalHorizontalSpeed / 2;
+		diverRigidbody.maxAngularVelocity = 100;
+		verticalSpeed = diverProps.trickSpinSpeed / 100;
+
+
+		controllingDiver = false;
+
+		//Set up platform
+		platform = (GameObject)Instantiate(platformGameObject,platformGameObject.transform.position,Quaternion.identity);
+
+		//Set up jumper
+		jumperPos = platform.transform.FindChild ("JumperPos").position;
+		jumper = (GameObject)Instantiate(jumperGameObject,jumperPos,Quaternion.identity);
+		jumperRigidbody = jumper.GetComponent<Rigidbody> ();
+
+		controllingJumper = true;
 	}
 
 	void SetDiverSpinSpeed(float normalSpin, float trickSpin)
@@ -80,7 +128,7 @@ public class GameController : MonoBehaviour
 	void ResetDiverPositionRotation()
 	{
 		Debug.Log (Mathf.Round (flips));
-		diver.onGround = true;
+		diverProps.onGround = true;
 		jumpEnd = Time.time;
 
 		//Debug.Log (diverRigidbody.position.x);
@@ -88,8 +136,8 @@ public class GameController : MonoBehaviour
 
 		diverRigidbody.velocity = Vector3.zero;
 		diverRigidbody.angularVelocity = Vector3.zero;
-		diverRigidbody.rotation = diverInitialRotation;
-		diverRigidbody.position = diverInitialPosition;
+		diverRigidbody.rotation = diverPos.rotation;
+		diverRigidbody.position = diverPos.position;
 	}
 
 	void DiverJump(Vector3 jumpForce)
@@ -101,8 +149,8 @@ public class GameController : MonoBehaviour
 
 	void CalculateFlips()
 	{
-		deltaRotation = (currentRotation - diver.transform.eulerAngles.z);
-		currentRotation = diver.transform.eulerAngles.z;
+		deltaRotation = (currentRotation - diverProps.transform.eulerAngles.z);
+		currentRotation = diverProps.transform.eulerAngles.z;
 		if (deltaRotation >= 300) 
 			deltaRotation -= 360;
 		if (deltaRotation <= -300) 
@@ -110,5 +158,12 @@ public class GameController : MonoBehaviour
 		WindupRotation += (deltaRotation);
 
 		flips = WindupRotation / 360;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Jumper"){
+			controllingDiver = true;
+			DiverJump (diverJumpForce);
+		};
 	}
 }
