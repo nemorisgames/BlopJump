c935530652f0ae54078d143a92f578c614430581
SLEIVA <sleiva@nemorisgames.com>
2016-12-03 14:57:16 -0300

    spawner de monedas en el landingspot..

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,10 +5,17 @@ public class Coin : MonoBehaviour {
 
 	public int value;
 	MainController controller;
+    Transform t;
 
 	void Awake(){
 		controller = GameObject.FindGameObjectWithTag ("MainController").GetComponent<MainController>();
+        t = GetComponent<Transform>();
 	}
+
+    void Update()
+    {
+        t.Rotate(Vector3.right * 50  * Time.deltaTime);
+    }
 	
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Diver") {
