5c9698ebd981d910610b50eba4ac4b536dda4d6e
nemorisgames <contacto@nemorisgames.com>
2016-12-16 17:40:29 -0300

    nuevo escenario

---
+++ Assets/Scripts/BackgroundChange.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundChange : MonoBehaviour {
+	public GameObject[] fondos;
+	public Material[] skyboxes;
+	int indice = 0;
+	// Use this for initialization
+	void Start () {
+		//change ();
+	}
+
+	public void change(){
+		indice++;
+		for (int i = 0; i < fondos.Length; i++) {
+			fondos [i].SetActive (i == indice);
+		}
+		RenderSettings.skybox = skyboxes[indice];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
